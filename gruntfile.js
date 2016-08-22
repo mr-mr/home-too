f64b0fa15c7580a91d008497c52d01d31b87bfe4
Chris J Clarke <mrmr.mail@gmail.com>
2016-08-22 17:20:17 +0100

    initial styles and svg added

---
+++ gruntfile.js
@@ -0,0 +1,79 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    sass: {
+      dist: {
+        files: {
+          'css/global.css' : 'src/_styles/global.scss'
+        }
+      }
+    },
+    connect: {
+      server: {
+        options: {
+          port: 8000,
+          hostname: '*',
+          onCreateServer: function(server, connect, options) {
+            var io = require('socket.io').listen(server);
+            io.sockets.on('connection', function(socket) {
+              // do something with socket
+            });
+          }
+        }
+      }
+    },
+    watch: {
+      css: {
+        files: '**/*.scss',
+        tasks: ['sass']
+      },
+      scripts: {
+          files: '**/*.js',
+          tasks: ['uglify']
+      }
+    },
+    cssmin: {
+      target: {
+        files: [{
+          expand: true,
+          cwd: 'style',
+          src: ['global.css', '!global.min.css'],
+          dest: 'build',
+          ext: '.min.css'
+        }]
+      }
+    },
+    uglify: {
+            dist: {
+                files:{
+                    'js/content.min.js': 'src/_scripts/content.js',
+                }
+            }
+        },
+    'ftp-deploy' : {
+      build: {
+        auth: {
+          host: 'mr-mr.co.uk',
+          port: 21,
+          authKey: 'key1'
+        },
+        src: '../home-too',
+        dest: '/hometoo',
+        exclusions: ['.DS_Store', 'tmp', '.ftppass', '.git']
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-notify');
+  grunt.loadNpmTasks('grunt-execute');
+  grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-ftp-deploy');
+
+
+  grunt.registerTask('default',['sass', 'connect', 'watch', 'cssmin', 'uglify']);
+}
--- index.js
+++ index.js
@@ -1,1 +1,2 @@
-console.log('hello');
+const circle = require('../js/circle.js');
+console.log(`The area of a circle of radius 4 is ${circle.area(4)}`);
